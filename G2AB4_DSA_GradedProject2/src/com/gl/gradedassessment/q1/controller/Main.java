f9d20711f18a047d018102612fca423f97cf7a6e
sethiyasanskar63 <101315790+sethiyasanskar63@users.noreply.github.com>
2022-11-12 23:40:55 +0530

    Add files via upload

---
+++ G2AB4_DSA_GradedProject2/src/com/gl/gradedassessment/q1/controller/Main.java
@@ -0,0 +1,88 @@
+package com.gl.gradedassessment.q1.controller;
+
+import java.util.Scanner;
+
+import com.gl.gradedassessment.q1.assembly.AssemblyOfFloors;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		boolean flag = true;
+		Integer totalFloors = 0;
+
+		while (flag) {
+			try {
+				System.out.println("Enter the total number of floors in the building:");
+				totalFloors = sc.nextInt();
+				flag = false;
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				System.out.println("Invalid input only integer allowed.");
+				sc.next();
+				continue;
+			}
+			if (totalFloors < 1) {
+				System.out.println("Entered integer should be greater than 0.");
+				flag = true;
+			}
+		}
+
+		AssemblyOfFloors floor = new AssemblyOfFloors(totalFloors);
+
+		Integer[] sizeOfFloors = new Integer[totalFloors];
+
+		for (int i = 0; i < totalFloors; i++) {
+
+			try {
+				System.out.println("Enter the floor size given on day: " + (i + 1));
+				sizeOfFloors[i] = Integer.valueOf(sc.nextInt());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				i--;
+				System.out.println("Invalid input only integer allowed.");
+				sc.next();
+				continue;
+			}
+
+			int count = 0;
+			for (int j = 0; j < i; j++) {
+				if (sizeOfFloors[i] == sizeOfFloors[j]) {
+					count++;
+					break;
+				}
+			}
+
+			if (count > 0) {
+				System.out.println("Enter a distinct Integer.");
+				i--;
+				continue;
+			}
+
+			if (sizeOfFloors[i] < 1) {
+				System.out.println("Enter an integer greater than 0.");
+				i--;
+				continue;
+			}
+
+			if (sizeOfFloors[i] > totalFloors) {
+				System.out.println("Enter an integer less than or equal to " + totalFloors);
+				i--;
+				continue;
+			}
+		}
+
+		sc.close();
+
+		System.out.println("The order of construction is as follows:");
+		for (int i = 0; i < totalFloors; i++) {
+			System.out.println("Day: " + (i + 1));
+			floor.floorConstruction(sizeOfFloors[i]);
+			System.out.println();
+		}
+
+	}
+
+}
